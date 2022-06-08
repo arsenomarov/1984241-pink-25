ae8f997897fe513480aa653f5cb2105284152c42
arsenomarov <sen.77@mail.ru>
2022-06-08 10:55:24 +0300

    Правки бургер + editorconfig

--- source/js/script.js
+++ source/js/script.js
@@ -1,7 +1,9 @@
 var navMain = document.querySelector('.main-nav');
+var header = document.querySelector('.main-header');
 var navToggle = document.querySelector('.main-header__toggle');
 
-navMain.classList.remove('main-nav--nojs');
+header.classList.remove('main-header--nojs');
+navMain.classList.add('main-nav--closed');
 
 navToggle.addEventListener('click', function() {
   if (navMain.classList.contains('main-nav--closed')) {
